a5bedf081f4862e163a01d45b5a0ec63b17463dc
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-23 16:29:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 function convertToRoman(num) {
-    const obj = {
-        0: ['M', 1000],
-        1: ['D', 500],
-        2: ['C', 100],
-        3: ['L', 50],
-        4: ['X', 10],
-        5: ['V', 5],
-        6: ['I', 1]
+    const romanSymbols = {
+        1000: ['M'],
+        900: ['CM'],
+        500: ['D'],
+        400: ['CD'],
+        100: ['C'],
+        90: ['XC'],
+        50: ['L'],
+        40: ['XL'],
+        10: ['X'],
+        9: ['IX'],
+        5: ['V'],
+        4: ['IV'],
+        1: ['I']
     };
+
+    const orderedValues = Object.keys(romanSymbols).sort((a, b) => b - a);
 
     let result = '';
 
-    for (let key in obj) {
-        const [symbol, value] = obj[key];
+    orderedValues.forEach(value => {
+        value = parseInt(value);
         while (num >= value) {
-            result += symbol;
+            result += romanSymbols[value][0];
             num -= value;
         }
-
-        // Handle subtractive notation (e.g., IV, IX, XL, XC, etc.)
-        const nextKey = (parseInt(key) + 1).toString();
-        if (obj[nextKey] && num >= value - obj[nextKey][1]) {
-            result += obj[nextKey][0] + symbol;
-            num -= (value - obj[nextKey][1]);
-        }
-    }
+    });
 
     return result;
 }
 
-// Test the function
+// Test the function 
 console.log(convertToRoman(36));
